710b93e0ca165686e14967be6b4ccd9604ca3fc3
pablo felipe <pabloletiane@gmail.com>
2025-11-24 16:56:20 -0300

    keys

--- my-app/services/parseConfig.ts
+++ my-app/services/parseConfig.ts
@@ -5,8 +5,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 Parse.setAsyncStorage(AsyncStorage);
 
 
-const PARSE_APPLICATION_ID = "app id";
-const PARSE_JAVASCRIPT_KEY = "key js ";
+const PARSE_APPLICATION_ID = "tWqOrszO8fAm6ZiUKzw6KTpikRGqtlNjiDDe7ORN";
+const PARSE_JAVASCRIPT_KEY = "MYXogIlx9NAV4Fe7aj3FC4hxAM8l5mnDD4cBwYtV";
 
 Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
 // URL padrão do Back4App
