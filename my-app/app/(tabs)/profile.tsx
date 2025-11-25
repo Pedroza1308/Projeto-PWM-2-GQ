cfb70618e20d7626b91910ab363b2ef042cdce86
Nunno Wakiyama Diniz Carvalho <142551010+nunnowdc@users.noreply.github.com>
2025-11-25 05:23:53 +0000

    arrumando navegação do botão sair

--- my-app/app/(tabs)/profile.tsx
+++ my-app/app/(tabs)/profile.tsx
@@ -52,7 +52,7 @@ export default function ProfileScreen() {
           style: 'destructive',
           onPress: () => {
             logout();
-            router.replace('/');
+            router.replace('../index');
           }
         }
       ]
